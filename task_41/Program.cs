f8d921eec408fffd4ccb192d7494ee58c130e959
Katsiaryna <caelpi85@gmail.com>
2022-07-16 22:37:07 +0200

    add all

---
+++ task_41/Program.cs
@@ -0,0 +1,32 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+/* Console.WriteLine("Input count of numbers");
+int M = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [M];
+int count = 0;
+Console.WriteLine("Input numbers");
+for (int i = 0; i < M; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    if(array[i] > 0)
+    {
+        count++;
+    }
+    //count = array[i] > 0 ? count + 1 : count;
+}
+Console.WriteLine($"Count of positive numbers = {count}"); */
+
+Console.WriteLine("Input numbers separated by spase");
+string rawNumbers = Console.ReadLine();
+if (rawNumbers.Trim() == string.Empty)
+{
+    Console.WriteLine("You didn't input anything");
+    return;
+}
+int[] array = Array.ConvertAll(rawNumbers.Split(" "), int.Parse);
+int count = 0;
+//for (int i = 0; i < array.Length; i++)
+foreach (int number in array)
+{ 
+    count = number > 0 ? count + 1 : count; 
+}
+Console.WriteLine($"Count of positive numbers = {count}");
---
+++ task_43/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//  значения b1, k1, b2 и k2 задаются пользователем.
+
+
+int k1, k2, b1, b2, x0, y0;
+
+Console.WriteLine("First line coefficients:");
+
+Console.Write("k1 = ");
+k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("b1 = ");
+b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Second line coefficients:");
+
+Console.Write("k2 = ");
+k2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("b2 = ");
+b2 = Convert.ToInt32(Console.ReadLine());
+
+if(k1 == k2)
+{
+    Console.WriteLine("Lines are parallel");
+    return;
+}
+
+x0 = (b2 - b1) / (k1 - k2);
+y0 = (k1 * b2 - k2 *b1) / (k1 -k2);
+
+
+Console.WriteLine($"Intersection: ({x0}, {y0})");
